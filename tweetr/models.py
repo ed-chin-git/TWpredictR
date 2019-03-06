24ba8e35dea4302d077a244211f5efa299f37829
ed.chin.git <edgardochin@gmail.com>
2019-03-06 08:25:17 -0600

    End od Day 2 commit

--- tweetr/models.py
+++ tweetr/models.py
@@ -7,7 +7,7 @@ class User(DB.Model):
     """Twitter users that we pull and analyze Tweets for."""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
-    #newest_tweet_id = DB.Column(DB.BigInteger)
+    newest_tweet_id = DB.Column(DB.BigInteger)
     def __repr__(self):
         return '<User {}>'.format(self.name)
 
@@ -16,7 +16,7 @@ class Tweet(DB.Model):
     id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(500))
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
-    #embedding = DB.Column(DB.PickleType, nullable=False)
+    embedding = DB.Column(DB.PickleType, nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
--- tweetr/twitter.py
+++ tweetr/twitter.py
@@ -9,3 +9,4 @@ TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY'),
 TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),
                               config('TWITTER_ACCESS_TOKEN_SECRET'))
 TWITTER = tweepy.API(TWITTER_AUTH)
+BASILICA = basilica.Connection(config('BASILICA_KEY')) 
