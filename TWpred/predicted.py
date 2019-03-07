d7d9a86a9d0b9b3fc3d0889df8c92abfd6276771
ed.chin.git <edgardochin@gmail.com>
2019-03-07 16:06:47 -0600

    fixed labeled 1/0 to 0/1 in predict.py

--- TWpred/predicted.py
+++ TWpred/predicted.py
@@ -15,8 +15,8 @@ def predict_user(user1_name, user2_name, tweet_text):
     user1_embeddings = np.array([tweet.embedding for tweet in user1.tweets])
     user2_embeddings = np.array([tweet.embedding for tweet in user2.tweets])
     embeddings = np.vstack([user1_embeddings, user2_embeddings])
-    labels = np.concatenate([np.ones(len(user1.tweets)),
-                             np.zeros(len(user2.tweets))])
+    labels = np.concatenate([np.zeros(len(user1.tweets)),
+                             np.ones(len(user2.tweets))])
     log_reg = LogisticRegression().fit(embeddings, labels)
     tweet_embedding = BASILICA.embed_sentence(tweet_text, model='twitter')
 
