c7fae35c7810400c7855ca4a09bb6950dce25eaa
ed.chin.git <edgardochin@gmail.com>
2019-09-25 14:24:52 -0500

    doc changes

--- TWpred/__init__.py
+++ TWpred/__init__.py
@@ -1,4 +1,6 @@
-""" entry point for flask app -> tweetr:APP """
+    """ entry point for flask -> TWpred:APP
+(sub directory location of __init__.py):(name of instantiated app Obj)  
+"""
 from .app import create_app
 
 APP = create_app()
